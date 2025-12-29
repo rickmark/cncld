25f1d00e3a57cffe8b76d5b13c7c97cb66fd4e1a
Rick Mark <rickmark@outlook.com>
2025-12-29 14:22:42 -0700

    Fix fonts / colors

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [searchTerm, setSearchTerm] = useState('')
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black">
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
       <div className="text-center">
         <h1 className="text-3xl font-bold mb-4">
           Is{' '}
@@ -15,7 +15,7 @@ export default function Home() {
             type="text"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
-            className="bg-transparent border-0 border-b-2 border-white text-white placeholder-white text-3xl font-bold text-center w-48 focus:outline-none"
+            className="bg-transparent border-0 border-b-2 border-white placeholder-white text-3xl font-bold text-center w-48 focus:outline-none"
           />{' '}
           cncld?
         </h1>
